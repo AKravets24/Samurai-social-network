4ad8112e849fead45c551759222b5f671ee61282
AKravets24 <a.kravets24@mail.ru>
2021-03-25 17:26:43 +0300

    added funnyLoader

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import stl from './App.module.css';
 import { HeaderContainer } from './header/header';
@@ -50,14 +50,97 @@ let AppContainer = () => {
 }
 
 type AppProps_Type = { appInitialized: boolean, auth_LDR_GIF: string }
+
+// let funnyLoaderArr = [
+//   'tectonic configuration...', 'filling the oceans...',
+//   'planting flora...', 'fauna breeding...',
+//   'crusades...', 'witch-hunting...',
+//   'transition into the renaissance era...',
+//   'scientific and technological revolution...',
+//   'Client: Synchronization...',] as string[];
+
+// function* funnyLoader(arr: string[]) {
+//   for (let i = 0; i < arr.length; i++) {
+//     yield arr[i]
+//   }
+// }
+// let num = funnyLoader(funnyLoaderArr)
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+
+
+// function* myGen() {
+//   let counter = 0;
+//   for (let i = 0; i <= 10; i++) {
+//     yield counter++;
+//   }
+// }
+
+// const gen = myGen();
+// const start = setInterval(() => {
+//   let next = gen.next();             // *** Save it here
+//   if (next.done) {                     // *** Use it here...
+//     clearInterval(start);
+//   } else {
+//     console.log(next.value);       // *** ...and here
+//   }
+// }, 1500)
+
+
 let App: React.FC<AppProps_Type> = ({ appInitialized, auth_LDR_GIF }) => {
+
+
+  let funnyLoaderArr = [
+    'tectonic configuration...', 'filling the oceans...',
+    'planting flora...', 'fauna breeding...',
+    'crusades...', 'witch-hunting...',
+    'transition into the renaissance era...',
+    'scientific and technological revolution...',
+    'Client: Synchronization...',] as string[];
+
+  let [loadPhrase, setLoadPhrase] = useState(funnyLoaderArr[0])
+
+
+  function* funnyLoader(loadersArr: string[]) {
+    for (let i = 0; i <= loadersArr.length; ++i) {
+      yield loadersArr[i]
+    }
+  }
+
+  let [refreshContent, setRefreshContent] = useState<any>();
+  let someFunc = (condition: boolean) => {
+
+    if (!condition) {
+      let num = funnyLoader(funnyLoaderArr)
+      setRefreshContent(setInterval(() => {
+        console.log(num.next())
+        //@ts-ignore
+        setLoadPhrase(num.next().value)
+        console.log('pagerefresh');
+      }, 1000)
+      )
+    }
+    else {
+      setTimeout(() => {
+        console.log('nofresh');
+        clearInterval(refreshContent)
+      }, 2000)
+    }
+  }
+
+  useEffect(() => { someFunc(appInitialized) }, [appInitialized])
+
+
   return <>
     <StoreContext.Consumer>
       {() => {
         return !appInitialized ?
           <div className={stl.loaderBlock}>
             <img src={auth_LDR_GIF} alt="err" />
-            <h1>Client: Synchronization...</h1>
+            {/* <h1>Client: Synchronization...</h1> */}
+            <h1>{loadPhrase}</h1>
           </div>
           :
           <div className={stl.container}>
